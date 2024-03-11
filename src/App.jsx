2e1c5d12f670c0141e823541137c8e560bfb2d52
Max de Vocht <maxdevocht@hotmail.com>
2024-03-11 19:21:21 +0100

    Update app.jsx
    added fuction

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,56 @@
+import { useState } from "react"
 import "./styles.css"
 
 export default function App() {
+  const [newItem, setNewItem] = useState("")
+  const [todos, setTodos] = useState([])
+
+  function handleSubmit(e) {
+    e.preventDefault()
+
+    setTodos(currentTodos => {
+      return [
+        ...currentTodos,
+        { id: crypto.randomUUID(), title: newItem, completed: false },
+      ]
+    })
+
+    setNewItem("")
+  }
+
+  function toggleTodo(id, completed) {
+    setTodos(currentTodos => {
+      return currentTodos.map(todo => {
+        if (todo.id === id) {
+          return { ...todo, completed }
+        }
+        return todo
+      })
+    })
+  }
+
   return (
     <>
-    <form className="new-item-form">
+    <form onSubmit={handleSubmit} className="new-item-form">
       <div className="form-row">
         <label htmlFor="item">New Item</label>
-        <input type="text" id="item" />
+        <input value={newItem} onChange={e => setNewItem(e.target.value)} type="text" id="item" />
       </div>
       <button className="btn">Add Item</button>
     </form>
     <h1 className="header">ToDo List</h1>
     <ul className="list">
-      <li>
-        <label htmlFor="">
-          <input type="checkbox" />
-          Item 1
-        </label>
-        <button className="btn btn-danger">Delete</button>
-      </li>
-      <li>
-        <label htmlFor="">
-          <input type="checkbox" />
-          Item 2
-        </label>
-        <button className="btn btn-danger">Delete</button>
-      </li>
+      {todos.map(todo => {
+        return (
+          <li key={todo.id}>
+            <label htmlFor="">
+            <input type="checkbox" checked={todo.completed} onChange={e => toggleTodo(todo.id, e.target.checked)}/>
+              {todo.title}
+            </label>
+            <button className="btn btn-danger">Delete</button>
+          </li>
+        )
+      })}
     </ul>
     </>
   )
